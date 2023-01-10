df9ce967e97d1e654fae4b81c6a7bfa2c719cd41
Pratyush Mishra <pratyushmishra287@gmail.com>
2023-01-10 21:08:09 +0530

    Add files via upload

---
+++ JS/conditions.js
@@ -0,0 +1,29 @@
+// if operator or conditional operator 
+// let time = prompt('what is the entering time?')
+// if (time < 10) {
+//    alert('you can come to class');
+// }else if(time<11){
+//    alert('you can come to class but stand outside');
+// }else{
+//    alert('go back home');
+// }
+
+
+// let accessAllowed;
+// let age = prompt('how old are you?')
+// console.log(age)
+// if (age>18){
+//    alert('access is allowed')
+// }else{
+//    alert('access not allowed')
+// }
+
+
+
+
+
+//let output = condition ? value1 : value2
+
+// let accessAllowed = age > 18 ?
+// 'access is allowed' : 'access is not allowed'
+// alert(accessAllowed)
---
+++ JS/variables.js
@@ -0,0 +1,43 @@
+// js was created to make web pages alive
+// write js in html and it runs as the page loads
+// js specification is ECMAScript 
+console.log('hello Vikas')
+
+// Variables 
+// it is named storage 
+
+
+// to create a variable in js use let keyword  
+
+let message; //variable declaration
+
+//initialize
+message = 'hello'
+message =10
+
+
+console.log(message)
+console.log(typeof message)
+
+alert(message)
+
+let age = prompt('please enter your age')
+console.log(age)
+
+var myName = 'pratyush'
+console.log(myName)
+
+//  const variable they cannot be reassigned 
+
+
+// interaction 
+// alert,prompt,confirm 
+
+let isProgrammer = confirm('Are you a programmer ?')
+console.log(isProgrammer)
+
+
+
+
+
+
